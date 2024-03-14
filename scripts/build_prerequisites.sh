98fc5333ab5e98066d2ed883a853ed5e2c518a88
ShekarKurva SaiChandra <shsaicha@silabs.com>
2024-03-14 19:15:55 +0530

    adujusting changes for automate amd64 libunify build

--- scripts/build_prerequisites.sh
+++ scripts/build_prerequisites.sh
@@ -6,10 +6,14 @@ export PATH=/usr/local/miniconda/bin:/usr/local/miniconda/condabin:/opt/cargo-ho
 
 cd /uic
 
-cmake -DCMAKE_INSTALL_PREFIX=$1/stage -GNinja -DCMAKE_TOOLCHAIN_FILE=$PWD/cmake/arm64_debian.cmake  -B build_unify_arm64/ -S components -DBUILD_TESTING=OFF
+if [ "$2" == "amd64" ]; then
+    cmake -DCMAKE_INSTALL_PREFIX=$1/stage -GNinja -B build_unify_$2/ -S components
+else
+    cmake -DCMAKE_INSTALL_PREFIX=$1/stage -GNinja -DCMAKE_TOOLCHAIN_FILE=$PWD/cmake/${2}_debian.cmake -B build_unify_$2/ -S components -DBUILD_TESTING=OFF
+fi
 
-cmake --build build_unify_arm64
+cmake --build build_unify_${2}
 
-cmake --install build_unify_arm64 --prefix $1/stage
+cmake --install build_unify_${2} --prefix $1/stage
 
 export PATH=$MATTER_ENV_PATH
