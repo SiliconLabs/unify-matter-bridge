9b67222c6dc8765c5bd6ccab3fe74e685ae86a5e
ShekarKurva SaiChandra <shsaicha@silabs.com>
2024-03-14 19:15:55 +0530

    adujusting changes for automate amd64 libunify build

--- scripts/build_prerequisites.py
+++ scripts/build_prerequisites.py
@@ -4,12 +4,18 @@ import sys
 
 def main():
     # Check if the expected number of command-line arguments is provided
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print("Usage: python build_prerequisites.py <root_build_dir>")
         sys.exit(1)
 
     # Extract the root_build_dir from the command-line arguments
     root_build_dir = sys.argv[1]
+
+    # Extract the architecture from command-line arguments
+    arch = sys.argv[2]
+
+    if arch == "x64":
+        arch = "amd64"
 
     # Get the file directory
     file_directory = os.path.dirname(os.path.abspath(__file__))
@@ -21,7 +27,7 @@ def main():
     script_path = os.path.join(file_directory, script_name)
 
     # Run the shell script with the provided root_build_dir argument
-    subprocess.run(['bash', script_path, root_build_dir])
+    subprocess.run(['bash', script_path, root_build_dir, arch])
 
 if __name__ == "__main__":
     main()
