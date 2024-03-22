308598e155133136a0a1ac68945f53830a53035a
sabollim <satyanaag.bollimpalli@silabs.com>
2024-03-22 18:49:56 +0530

    fixing Jenkinsfile issues

--- scripts/build_essentials.sh
+++ scripts/build_essentials.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Initialize the submodules
-git submodule update --init
+git submodule update --init 
 
 #Checks out linux submodules
 ./linux/third_party/connectedhomeip/scripts/checkout_submodules.py --platform linux
--- scripts/build_prerequisites.py
+++ scripts/build_prerequisites.py
@@ -10,6 +10,12 @@ def main():
 
     # Extract the root_build_dir from the command-line arguments
     root_build_dir = sys.argv[1]
+    print("The value of root_build_dir is: " + root_build_dir)
+
+    # Building the unify_path from the root_build_dir
+    path_components = root_build_dir.split('/')
+    unify_path_components = path_components[:-3] + ['third_party', 'UnifySDK']
+    unify_path = '/'.join(unify_path_components)
 
     # Extract the architecture from command-line arguments
     arch = sys.argv[2]
@@ -19,15 +25,17 @@ def main():
 
     # Get the file directory
     file_directory = os.path.dirname(os.path.abspath(__file__))
+    print("The value of file_directory is: " + file_directory)
 
     # Specify the name of your shell script
     script_name = 'build_prerequisites.sh'
 
     # Construct the full path to the shell script
     script_path = os.path.join(file_directory, script_name)
+    print("The value of script_path is: " + script_path)
 
     # Run the shell script with the provided root_build_dir argument
-    subprocess.run(['bash', script_path, root_build_dir, arch])
+    subprocess.run(['bash', script_path, root_build_dir, arch, unify_path])
 
 if __name__ == "__main__":
     main()
